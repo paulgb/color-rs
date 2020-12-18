e9b896ce28989a080b76ada865869f1435636105
Arturo Castro <arturo@rin.rs>
2020-12-18 17:28:58 +0100

    lab: added some features
    chromacity, luma, offset_chormacity, Clone, Copy, Debug, Mul and Add

--- src/lab.rs
+++ src/lab.rs
@@ -1,8 +1,10 @@
 use channel::Channel;
 use color_space::{WhitePoint};
-use num_traits::{Float, NumCast, cast};
+use num_traits::{Float, NumCast, cast, zero};
 use xyz::{Xyz, ToXyz};
+use std::ops::{Add, Mul};
 
+#[derive(Clone, Copy, Debug)]
 pub struct Lab<T, Wp>{
     pub l: T,
     pub a: T,
@@ -13,6 +15,39 @@ pub struct Lab<T, Wp>{
 impl<T, Wp: WhitePoint> Lab<T, Wp>{
     pub fn new(l: T, a: T, b: T) -> Lab<T, Wp>{
         Lab { l, a, b, white_point: Wp::default() }
+    }
+}
+
+
+impl<T: Copy, Wp: WhitePoint> Lab<T, Wp>{
+    pub fn brightness(&self) -> T {
+        self.l
+    }
+}
+
+impl<T: Float, Wp: WhitePoint> Lab<T, Wp>{
+    pub fn chromacity(&self) -> T {
+        (self.a.powi(2) + self.b.powi(2)).sqrt()
+    }
+
+    pub fn hue(&self) -> T {
+        let h = self.b.atan2(self.a);
+        if h < zero() {
+            h + cast(std::f64::consts::TAU).unwrap()
+        }else{
+            h
+        }
+    }
+
+    pub fn offset_chromacity(&self, chroma_offset: T) -> Lab<T, Wp>{
+        let current_croma = self.chromacity();
+        let offset_a = self.a / current_croma * chroma_offset;
+        let offset_b = self.b / current_croma * chroma_offset;
+        Lab::new(
+            self.l,
+            self.a + offset_a,
+            self.b + offset_b,
+        )
     }
 }
 
@@ -47,12 +82,17 @@ impl<T: Channel + Float + NumCast, Wp: WhitePoint> ToXyz for Lab<T, Wp> {
         let zr = if fz3 > e {
             fz3
         }else{
-            (fz - d) * k
-        };
-        let x = xr * Wp::xyz().x;
-        let y = yr * Wp::xyz().y;
-        let z = zr * Wp::xyz().z;
 
-        Xyz::new(x.to_channel(), y.to_channel(), z.to_channel())
+impl<T: Channel + Float + NumCast, Wp: WhitePoint> Add for Lab<T,Wp>{
+    type Output = Lab<T, Wp>;
+    fn add(self, other: Lab<T, Wp>) -> Lab<T, Wp> {
+        Lab::new(self.l + other.l, self.a + other.a, self.b + other.b)
+    }
+}
+
+impl<T: Channel + Float + NumCast, Wp: WhitePoint> Mul<T> for Lab<T,Wp>{
+    type Output = Lab<T, Wp>;
+    fn mul(self, other: T) -> Lab<T, Wp> {
+        Lab::new(self.l * other, self.a * other, self.b * other)
     }
 }
